a23850e4a89ea4149b08caed3f45e149febc81d5
kenchaaan <feyn.miya@gmail.com>
2019-10-12 09:27:06 +0900

    update

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import util
 import threading
 import importlib
 
-host = util.get_server_host()
-port = util.get_server_port()
-topic = util.get_topic()
-protocol = util.get_protocol()
-
 
 def call_class(protocol, host, port, topic):
     mod = importlib.import_module('{}_client'.format(protocol))
@@ -14,8 +9,15 @@ def call_class(protocol, host, port, topic):
     return mod(host, port, topic)
 
 
+host = util.get_server_host()
+port = util.get_server_port()
+topic = util.get_topic()
+protocol = util.get_protocol()
+thread_num = util.get_thread_num()
+
 client = call_class(protocol, host, port, topic)
-for i in range(2):
+
+for i in range(thread_num):
     t_send = threading.Thread(target=client.send_data, args=[i])
     t_receive = threading.Thread(target=client.receive_data, args=[i])
     t_send.start()
